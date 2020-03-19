0edc2a06ceb70e899e446eba6ff7a2cdcd785e7f
s19605 <s19605@pjwstk.edu.pl>
2020-03-19 10:06:15 +0100

    poczatek

---
+++ TPO2_BI_S19605/src/zad1/Main.java
@@ -0,0 +1,19 @@
+/**
+ *
+ *  @author Bałka Ignacy S19605
+ *
+ */
+
+package zad1;
+
+
+public class Main {
+  public static void main(String[] args) {
+    Service s = new Service("Poland");
+    String weatherJson = s.getWeather("Warsaw");
+    Double rate1 = s.getRateFor("USD");
+    Double rate2 = s.getNBPRate();
+    // ...
+    // część uruchamiająca GUI
+  }
+}
---
+++ TPO2_BI_S19605/src/zad1/Service.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Bałka Ignacy S19605
+ *
+ */
+
+package zad1;
+
+
+public class Service {
+}  
